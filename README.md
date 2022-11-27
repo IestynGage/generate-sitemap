47cbb8480e7f4eb28ce3962263cbafba2ce79259
Iestyn <>
2022-11-27 18:22:57 +0000

    explore all the files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Generate sitemap
+
+This project scans a directory and creates a `sitemap.xml` with all the html files.
+
+**sitemap** - A xml file that lists all the html files. This is used by web crawls to index every file on a website.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+#!/usr/bin/node
+
 var path = require('path');
 const fs = require('fs');
 
-const folder = path.dirname('./test')
-
 //protect against null/undefined
-fs.readdir(folder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-});
+
+const readFiles = (directory) => {
+  let files = []
+  const directoryStats = fs.lstatSync(directory);
+  if (directoryStats.isDirectory()) {
+    console.log("is directory", directory)
+    const folder = path.dirname(directory + "/.");
+    fs.readdir(folder, (err, files) => {
+      files.forEach(file => {
+        console.log("file", file)
+        files.concat(readFiles(directory + "/" + file))
+      });
+    });
+  } else {
+    console.log("is file", directory)
+    files[0] = directory;
+  }
+  return files;
+}
+
+const a = readFiles('src/test/resource/simpleExampleTwo');
+
+console.log(a)
